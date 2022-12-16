1b9ec3478e08894dafe5fb9588cbff123dc2fe4e
TinNguyenHuu <92506042+TinNguyenHuu01@users.noreply.github.com>
2022-12-16 14:55:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def index():
         # Nếu là GET thì hiển thị giao diện upload
       return render_template('index.html')
 if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=8080, debug=True)
+   app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
